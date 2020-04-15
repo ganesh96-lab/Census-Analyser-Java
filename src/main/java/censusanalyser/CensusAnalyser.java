e622a39defe82e2319a34fe7107ea0d68512b717
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-04-15 13:03:22 -0400

    REFACTOR:Code to use stream instead of while loop for loading state code

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -25,11 +25,10 @@ public class CensusAnalyser {
             csvFileMap=new HashMap<String,IndiaCensusCSV>();
             csvFileList=new ArrayList<IndiaCensusCSV>();
             Iterator<IndiaCensusCSV> indiaCensusCSVIterator=csvBuilder.getCSVFileIterator(reader,IndiaCensusCSV.class);
-            while (indiaCensusCSVIterator.hasNext()){
-                CensusDAO censusDAO=new CensusDAO(indiaCensusCSVIterator.next());
-                this.csvFileMap.put(censusDAO.getState(),censusDAO);
-                this.csvFileList=(List) csvFileMap.values().stream().collect(Collectors.toList());
-            }
+            Iterable<IndiaCensusCSV> indiaCensusCSVIterable = () -> indiaCensusCSVIterator;
+            StreamSupport.stream(indiaCensusCSVIterable.spliterator(),false)
+                    .forEach(indiaCensusCSV -> csvFileMap.put(indiaCensusCSV.state, new CensusDAO(indiaCensusCSV)));
+            this.csvFileList = (List) csvFileMap.values().stream().collect(Collectors.toList());
             return csvFileMap.size();
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
@@ -47,11 +46,10 @@ public class CensusAnalyser {
             csvFileMap=new HashMap<String,IndiaStateCodeCSV>();
             csvFileList=new ArrayList<IndiaStateCodeCSV>();
             Iterator<IndiaStateCodeCSV> indiaStateCodeCSVIterator=csvBuilder.getCSVFileIterator(reader,IndiaStateCodeCSV.class);
-            while (indiaStateCodeCSVIterator.hasNext()){
-                CensusDAO censusDAO=new CensusDAO(indiaStateCodeCSVIterator.next());
-                this.csvFileMap.put(censusDAO.getStateCode(),censusDAO);
-                this.csvFileList=(List) csvFileMap.values().stream().collect(Collectors.toList());
-            }
+            Iterable<IndiaStateCodeCSV> indiaStateCodeCSVIterable = () -> indiaStateCodeCSVIterator;
+            StreamSupport.stream(indiaStateCodeCSVIterable.spliterator(), false)
+                    .forEach(indiaStateCodeCSV -> csvFileMap.put(indiaStateCodeCSV.StateCode, new CensusDAO(indiaStateCodeCSV)));
+            this.csvFileList = (List) csvFileMap.values().stream().collect(Collectors.toList());
             return csvFileMap.size();
         } catch (IOException e){
             throw new CensusAnalyserException(e.getMessage(),
